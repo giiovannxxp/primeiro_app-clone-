e1fd775d52bb8e1dd4db739b66e46cecbf1227d6
Giovanna Lopes Santana <00001127510368sp@aluno.educacao.sp.gov.br>
2026-03-13 14:45:15 -0300

    update projeto

--- Primeiro_app/obj/Debug/net10.0/Primeiro_app.AssemblyInfo.cs
+++ Primeiro_app/obj/Debug/net10.0/Primeiro_app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primeiro_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c8755c2bf35748fe6457c7194f3212afcb387cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primeiro_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primeiro_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
